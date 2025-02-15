29d388f12d946aac58a286a520985b52bca58b57
sanmitc <sanmitc@iisc.ac.in>
2025-02-15 12:01:10 +0530

    cleaned up si_scf_supercell

--- examples/dft-si/si_scf_supercell.py
+++ examples/dft-si/si_scf_supercell.py
@@ -73,79 +73,87 @@ reallat = RealLattice.from_alat(
     alat, a1=[-0.5, 0.0, 0.5], a2=[0.0, 0.5, 0.5], a3=[-0.5, 0.5, 0.0]  # Bohr
 )
 
-def generate_coordinates(n):
-    num_atoms = 2 * n**3
-    min_distance = 1 / (4 * n)
-    coordinates = []
-    coordinates=np.zeros((num_atoms, 3))
-    for inum in range(num_atoms):
-        new_coord = np.random.uniform(-1, 1, 3)
-        for iprev in range(inum):
-            coord = coordinates[iprev]
-            if np.linalg.norm(new_coord-coord) >= min_distance :
-                coordinates[inum] = new_coord 
-    return np.array(coordinates)
-
-data= [
-[0.00000000e+00,0.00000000e+00,0.00000000e+00],
-[-1.26874010e-01,-4.79302744e-01,-6.10759530e-01],
-[4.47320598e-01,3.56068727e-01,2.14939978e-01],
-[7.35261283e-01,7.10631374e-01,-1.71979919e-01],
-[5.95178903e-01,-2.99780556e-01,6.22027236e-01],
-[3.21705506e-01,-6.33604345e-01,4.37190459e-01],
-[-5.01662582e-01,-4.59022612e-01,-1.28027812e-01],
-[4.61663485e-01,-5.61124891e-02,7.22458111e-04],
-[-1.82235284e-01,1.24419831e-01,6.43527637e-01],
-[6.31703350e-01,-6.59393935e-01,-7.15236381e-01],
-[-6.01760002e-01,2.63670200e-01,-3.98291936e-01],
-[-6.42023476e-01,-3.34217437e-01,-2.19091562e-01],
-[6.75952042e-01,3.08814554e-01,-6.12721091e-01],
-[-4.72891793e-02,1.89579113e-01,7.05939113e-02],
-[4.91181274e-02,3.31468657e-01,1.22516957e-01],
-[1.66439643e-01,-9.75885442e-02,-2.96180613e-01],
-[-1.53434519e-01,6.47267116e-01,6.13048518e-01],
-[7.82260133e-01,-2.58914723e-01,-6.91194930e-01],
-[9.17270059e-02,3.85267675e-01,1.75586629e-01],
-[5.16319462e-01,3.99666219e-01,-5.37132905e-01],
-[1.23239508e-02,-1.63366082e-01,1.61514815e-01],
-[3.55968704e-01,4.44123528e-01,6.33628585e-01],
-[-6.38712643e-01,3.17482231e-02,5.05398707e-01],
-[6.45656232e-01,7.57919233e-01,5.87418505e-01],
-[-6.48687427e-01,-2.05626403e-01,2.85428698e-01],
-[4.16671196e-02,4.12311225e-01,-5.23716825e-02],
-[-3.59707128e-02,5.57446126e-03,-4.93162593e-01],
-[-2.68437676e-01,3.61170634e-01,7.61734028e-01],
-[-1.37912657e-01,2.03259373e-01,7.62316736e-01],
-[2.95525483e-01,6.14261199e-01,5.29172992e-01],
-[7.17401831e-01,5.62752441e-01,6.73835043e-01],
-[-7.64983377e-01,-1.11698092e-01,-2.32283729e-01],
-[-5.32217701e-01,-7.77385181e-01,-3.35421955e-01],
-[5.09004378e-01,-2.90370382e-01,-3.24567542e-01],
-[7.34674423e-01,2.04810486e-01,6.71424893e-01],
-[4.45261665e-01,-6.07089439e-01,4.40525180e-01],
-[7.06776717e-01,-6.81888855e-01,-9.76909291e-02],
-[1.85421657e-01,-5.52565673e-01,4.78136379e-01],
-[1.77263301e-01,6.26317314e-01,-2.23102596e-01],
-[4.38386462e-01,-4.39912938e-01,-6.72237534e-01],
-[4.19372251e-01,2.35118545e-01,-5.84648465e-03],
-[3.70444245e-01,6.39836967e-01,4.14212345e-01],
-[6.10804811e-01,-6.85500886e-01,6.69760035e-02],
-[-1.06243850e-01,4.34830317e-01,-3.64171982e-01],
-[1.19664058e-01,-2.56958474e-01,2.69747990e-01],
-[-1.90562858e-01,1.14657245e-01,4.64925890e-01],
-[4.97001563e-01,-2.97593832e-01,-3.77250708e-01],
-[6.72481274e-01,2.16678025e-01,1.79163605e-01],
-[5.37797379e-02,-2.94783523e-01,-7.03673869e-01],
-[-4.20653006e-04,-6.08445985e-01,2.34014405e-01],
-[3.93813294e-01,7.40272951e-01,-3.28358860e-01],
-[-6.66284173e-02,1.95279253e-01,-2.85664231e-01],
-[5.01856874e-01,-1.33779730e-01,-4.53433005e-01],
-[4.16701642e-01,6.61833568e-01,-4.55031196e-01]]
-
-N=np.array(data)
 # Atom Basis
 si_oncv = UPFv2Data.from_file("Si_ONCV_PBE-1.2.upf")
-si_atoms = BasisAtoms.from_alat(
+
+si_atoms = BasisAtoms(
+    "si",
+    si_oncv,
+    28.086,
+    reallat,
+    np.array([[0.875, 0.875, 0.875], [0.125, 0.125, 0.125]]).T,
+)
+
+crystal_unit = Crystal(reallat, [si_atoms]) 
+crystal_supercell=crystal_unit.gen_supercell([supercell_size] * 3)
+##print the crystal coordinates of the supercell
+#print("The crystal coordinates of the supercell", crystal_supercell.l_atoms[0].r_alat)
+r_alat_supercell=crystal_supercell.l_atoms[0].r_alat.T
+
+#print("the original coordinates are", r_alat_supercell)
+
+data=[[ 6.58943879e-04,6.91497266e-04,7.48515032e-05],
+ [-6.05728097e-05, -8.75943473e-04, -7.04679950e-04],
+ [-6.10954497e-04, -7.56690520e-04, -8.03117566e-04],
+ [-9.45313375e-04, -9.18127002e-04,4.66155557e-04],
+ [-5.54215340e-04, -3.11150267e-04,6.97699613e-04],
+ [ 4.70218205e-04, -2.94599103e-04,5.82396611e-04],
+ [ 1.34223693e-04,5.39662464e-04, -7.56884917e-04],
+ [ 1.46562007e-04, -4.75724968e-04,3.49339605e-05],
+ [ 3.69045709e-05,5.57544205e-04, -2.28853210e-04],
+ [-9.00268630e-05, -5.47027040e-04,1.76136806e-04],
+ [ 4.61637614e-04, -9.79828799e-04, -1.53985018e-04],
+ [-2.81704763e-04,9.31582408e-04,8.38253258e-04],
+ [-2.83497159e-04, -4.75322115e-04, -2.09080901e-04],
+ [ 6.31048459e-04, -4.72329903e-04,8.38034461e-04],
+ [ 4.90094430e-04,5.37888451e-04,1.71644742e-04],
+ [ 1.10833347e-04,8.81252041e-05,5.23560885e-04],
+ [-5.04979146e-04,4.49471438e-04,5.75469663e-04],
+ [-9.10478528e-05,8.85532305e-04,1.91818065e-04],
+ [ 4.73158124e-04, -9.14054407e-04, -6.20230266e-04],
+ [-7.51088497e-04,6.05364535e-04, -2.27549946e-04],
+ [ 1.47776811e-05,9.24451428e-04, -7.83801622e-04],
+ [ 6.93567368e-04, -2.98025129e-04, -3.18008522e-05],
+ [ 6.61267454e-04,2.22968662e-04,8.32347226e-04],
+ [-8.07183488e-04, -1.18307189e-04, -9.11752326e-04],
+ [-3.28766544e-04, -9.00191478e-04, -5.56297740e-04],
+ [-6.12949076e-04,4.61322052e-05, -6.24005589e-04],
+ [ 5.57522831e-04, -6.27987365e-04, -8.37730046e-04],
+ [-2.85304595e-04, -7.31672139e-04, -7.44141875e-04],
+ [-9.74106801e-04, -5.89766002e-04,6.74298368e-04],
+ [ 8.13531704e-05,1.91106733e-04, -8.15295801e-04],
+ [ 8.46892006e-04,9.03127092e-04, -2.45955077e-04],
+ [-6.59793927e-04,6.62838914e-04,4.50476010e-04],
+ [ 7.42760612e-04, -8.01320341e-04, -3.15969379e-04],
+ [ 8.52419875e-04, -9.60273012e-04,1.13681948e-04],
+ [-7.97482839e-04, -3.91599552e-04, -3.26885348e-04],
+ [ 3.98426986e-04,6.32619791e-04,5.24122998e-04],
+ [ 3.78839087e-04, -4.99125943e-04,6.51472900e-05],
+ [ 1.25583492e-04,6.68896747e-04, -8.03354387e-05],
+ [ 3.94636444e-04, -1.14553966e-04, -1.74867050e-04],
+ [-5.76888111e-04, -6.50713363e-04, -3.05995101e-04],
+ [ 9.81393712e-04, -8.63231993e-04, -9.23577518e-04],
+ [-4.37174688e-04,9.40450698e-04, -5.56705373e-04],
+ [-3.84127055e-04,2.60384611e-05,1.92124696e-04],
+ [ 4.15749460e-04, -4.67585213e-04, -8.85328530e-04],
+ [ 9.42300802e-04, -2.92518048e-04, -2.63869872e-04],
+ [ 7.44418850e-04, -9.10014530e-04,9.49943459e-05],
+ [-6.26709933e-04, -3.45556276e-04,4.59098437e-04],
+ [-5.09495385e-04,6.92071890e-04,6.30685829e-04],
+ [ 6.63648691e-04, -1.68377664e-04,7.66466740e-04],
+ [ 5.65275529e-05,9.75354510e-04, -7.27349789e-04],
+ [-3.82731670e-04, -6.25633060e-04, -1.78607492e-04],
+ [-4.04577925e-04, -6.72948025e-05,4.37152063e-04],
+ [ 1.58613273e-04,1.07264431e-04, -9.99014418e-04],
+ [-9.95646352e-04, -2.10658918e-04,9.96020775e-04]]
+
+data=np.array(data)
+
+N=r_alat_supercell+ 1*data
+
+#print("the new coordinates in alat units", N)
+
+si_atoms_supercell = BasisAtoms.from_alat(
     "si",
     si_oncv,
     28.086,
@@ -153,30 +161,25 @@ si_atoms = BasisAtoms.from_alat(
     N,
 )
 
-crystal = Crystal(reallat, [si_atoms])  # Represents the crystal
+crystal = Crystal(reallat, [si_atoms_supercell]) 
+
+ # Represents the crystal
 
 #crystal = crystal.gen_supercell([supercell_size] * 3)
-si_basis=crystal.l_atoms[0]
 ##We want to print the coordinates of the Si atms
 #print("Si basis", si_basis.r_alat)
-
 #coordinates=generate_coordinates(supercell_size).T
 ## Set this as the new coordinates of the basis
 #si_basis.r_cart=coordinates
-
-
 #print("new coordinates", si_basis.r_cart)
-
-
-
 # Generating k-points from a Monkhorst Pack grid (reduced to the crystal's IBZ)
 mpgrid_shape = (1, 1, 1)
 mpgrid_shift = (False, False, False)
 kpts = gen_monkhorst_pack_grid(crystal, mpgrid_shape, mpgrid_shift)
 
 # -----Setting up G-Space of calculation-----
-ecut_wfn = 15 * RYDBERG
-ecut_rho = 4 * ecut_wfn
+ecut_wfn = 10 * RYDBERG
+ecut_rho = 4 *ecut_wfn
 grho_serial = GSpace(crystal.recilat, ecut_rho)
 
 # If G-space parallelization is not required, use the serial G-space object
@@ -190,7 +193,7 @@ gwfn = grho
 
 print("the type of grho is", type(grho))    
 
-numbnd = crystal.numel // 2  # Ensure adequate # of bands if system is not an insulator
+numbnd = int(2.5*(crystal.numel // 2)) # Ensure adequate # of bands if system is not an insulator
 conv_thr = 1e-8 * RYDBERG
 diago_thr_init = 1e-2 * RYDBERG
 
@@ -256,6 +259,7 @@ if dftcomm.image_comm.rank==0:
 print(flush=True)
 
 #force_time=time.time()
+'''start_time = time.time()
 force_total, force_norm=force(dftcomm=dftcomm,
                             numbnd=numbnd,
                             wavefun=l_wfn_kgrp,
@@ -268,7 +272,16 @@ force_total, force_norm=force(dftcomm=dftcomm,
                             verbosity=True)
 
 
+if dftcomm.image_comm.rank==0:
+    print("force total", force_total)
+    print("force norm", force_norm)
+    print("Time taken for force: ", time.time() - start_time)'''
 
 if comm_world.rank == 0:
     print("SCF Routine has exited")
     print(qtmlogger)
+
+final_time=time.time() 
+
+if dftcomm.image_comm.rank==0:
+    print("Total time taken for the calculation", final_time-initial_time)
