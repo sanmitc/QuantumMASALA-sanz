cdba3126a1f3b840eea8d15be877cc325e81294f
sanmitc <sanmitc@iisc.ac.in>
2025-04-03 15:06:57 +0530

    Added Andersen thermostat options

--- src/qtm/MD/andersen.py
+++ src/qtm/MD/andersen.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import gc
 
 import numpy as numpy
 
@@ -103,8 +104,9 @@ def Andersen_MD(dftcomm: DFTCommMod,
           dt: float,
           T_init: float,
           nu: float,
-          kgrid:tuple[int, int, int],
-          kshift: tuple[bool, bool, bool],
+          kpts:KList,
+          grho: GSpace,
+          gwfn: GSpace,
           ecut_wfn:float,
           numbnd:int,
           is_spin:bool,
@@ -135,11 +137,11 @@ def Andersen_MD(dftcomm: DFTCommMod,
         ppdat_cryst=np.array([sp.ppdata for sp in l_atoms])
         label_cryst=np.array([sp.label for sp in l_atoms])
         mass_cryst=np.array([sp.mass for sp in l_atoms])*M_NUC_HART
-        mass_all=np.repeat([sp.mass for sp in l_atoms], [sp.numatoms for sp in l_atoms]).reshape(-1,1)*M_NUC_HART
-        tot_mass=np.sum(mass_all)
+        mass_all=np.repeat([sp.mass for sp in l_atoms], [sp.numatoms for sp in l_atoms])*M_NUC_HART
+        #tot_mass=np.sum(mass_all)
         num_typ = len(l_atoms)
         reallat=crystal.reallat
-        lnum_labels = np.repeat([np.arange(num_typ)], [sp.numatoms for sp in l_atoms])
+        #lnum_labels = np.repeat([np.arange(num_typ)], [sp.numatoms for sp in l_atoms])
         #coords_alat_all = np.concatenate([sp.r_alat for sp in l_atoms], axis=1)
         coords_cart_all = np.concatenate([sp.r_cart for sp in l_atoms], axis =1).T
         ##This is a numatom times 3 array containing the coordinates of all the atoms in the crystal
@@ -149,22 +151,23 @@ def Andersen_MD(dftcomm: DFTCommMod,
         mass_si=mass_all*MASS_SI
         ##First we assign velocities to the atoms
         vel=np.random.rand(tot_num, 3)-0.5
-        comm.Bcast(vel)
+        vel=comm.allreduce(vel)
+        vel/=comm.size
         ##Calculate the momentum
-        momentum=mass_si*vel
+        momentum=mass_si*vel.T
+        momentum_T=np.array(momentum).T
 
         ##Compute the momentum of the center of mass
-        momentum_cm=np.sum(momentum, axis=0)
+        momentum_T-=np.mean(momentum_T, axis=0)
 
-        ##Subtract the momentum of the center of mass from the momentum of the atoms
-        momentum-=momentum_cm
 
         ##Calculate the updated velocity of the atoms
-        vel=momentum/mass_si
+        vel=momentum_T.T/mass_si
+        vel=vel.T
 
         ##Calculate the kinetic energy
-        ke_init=0.5*np.sum(mass_si*np.sum(vel**2, axis=1))
-
+        ke_init=0.5*np.sum(mass_si*vel.T**2)
+        del momentum, momentum_T
         ##Calculate the temperature
         T=2*ke_init/(3*tot_num*BOLTZMANN_SI)
 
@@ -173,10 +176,9 @@ def Andersen_MD(dftcomm: DFTCommMod,
         ##Rescale the velocities to the desired temperature
         vel*=np.sqrt(T_init/T)
 
-
         #region Debug statement
         ##Calculate the kinetic energy
-        ke_init=0.5*np.sum(mass_si*np.sum(vel**2, axis=1))
+        ke_init=0.5*np.sum(mass_si*vel.T**2)
 
         ##Calculate the temperature
         T_later=2*ke_init/(3*tot_num*BOLTZMANN_SI)
@@ -187,9 +189,12 @@ def Andersen_MD(dftcomm: DFTCommMod,
         ##Convert the velocities to atomic units
         vel/=vel_HART
 
-        time_array=[]
-        energy_array=[]
-        temperature_array=[]
+        time_step=int(max_t/dt)
+        time_array = np.empty(time_step)
+        energy_array = np.empty(time_step)
+        ke_array=np.empty(time_step)
+        pe_array=np.empty(time_step)
+        temperature_array = np.empty(time_step)
 
         ##This computes the forces on the molecules
         def compute_en_force(dftcomm, coords_all, rho: FieldGType):
@@ -211,16 +216,9 @@ def Andersen_MD(dftcomm: DFTCommMod,
                                                     *coord_alat_sp)
                     l_atoms_itr.append(Basis_atoms_sp)
                 crystal_itr=Crystal(reallat, l_atoms_itr)
-                kpts_itr=gen_monkhorst_pack_grid(crystal_itr, kgrid, kshift, use_symm, is_time_reversal)
-
-                ecut_rho=4*ecut_wfn
-                grho_itr_serial=GSpace(crystal_itr.recilat, ecut_rho)
-                if dftcomm.n_pwgrp == dftcomm.image_comm.size:  
-                    grho_itr = grho_itr_serial
-                else:
-                    grho_itr = DistGSpace(comm_world, grho_itr_serial)
-                gwfn_itr=grho_itr
-                FieldG_rho_itr: FieldGType= get_FieldG(grho_itr)
+                #kpts_itr=gen_monkhorst_pack_grid(crystal_itr, kgrid, kshift, use_symm, is_time_reversal)
+                
+                FieldG_rho_itr: FieldGType= get_FieldG(grho)
                 if rho is not None: rho_itr=FieldG_rho_itr(rho.data)
                 else: rho_itr=rho
 
@@ -232,9 +230,9 @@ def Andersen_MD(dftcomm: DFTCommMod,
                 out = scf(
                         dftcomm=dftcomm, 
                         crystal=crystal_itr, 
-                        kpts=kpts_itr, 
-                        grho=grho_itr, 
-                        gwfn=gwfn_itr,
+                        kpts=kpts, 
+                        grho=grho, 
+                        gwfn=gwfn,
                         numbnd=numbnd, 
                         is_spin=is_spin, 
                         is_noncolin=is_noncolin, 
@@ -257,48 +255,62 @@ def Andersen_MD(dftcomm: DFTCommMod,
                         )
                 
                 scf_converged, rho, l_wfn_kgrp, en, v_loc, nloc, xc_compute = out
-                print("my rank is", dftcomm.image_comm.rank)
-                print("And I have successfully calculated energy", en.total)
+                if comm.rank==0:  
+                    print("my rank is", dftcomm.image_comm.rank)
+                    print("And I have successfully calculated energy", en.total)
                 #region of calculation of the jacobian i.e the force
 
                 force_itr= force(dftcomm=dftcomm,
                                     numbnd=numbnd,
                                     wavefun=l_wfn_kgrp, 
                                     crystal=crystal_itr,
-                                    gspc=gwfn_itr,
+                                    gspc=gwfn,
                                     rho=rho,
                                     vloc=v_loc,
                                     nloc_dij_vkb=nloc,
                                     gamma_only=False,
-                                    verbosity=False)[0]
-                
-                print("I am process", comm.rank, "and I have calculated the force", force_itr)
+                                    verbosity=True)[0]
 
+                del l_wfn_kgrp, v_loc, nloc, xc_compute, crystal_itr, FieldG_rho_itr
+                for var in list(locals().keys()):
+                    if var not in ["en", "force_itr", "rho"]:
+                        del locals()[var]
+                gc.collect()  
+                #if dftcomm.image_comm.rank==0:
+                    #print("I am process", comm.rank, "and I have calculated the force", force_itr)
             return en.total, force_itr, rho
         
         #Initial configuration of the system
-        en, force_coord, rho=compute_en_force(coords_cart_all, rho_start)
+        en, force_coord, rho=compute_en_force(dftcomm, coords_cart_all, rho_start)
+        force_coord*=RYDBERG
         rho_md=rho
         time=0
         while time<max_t:
             ## Starting of the Iterartion
             ## Calculating accelaration
-            accelaration=force_coord/mass_all
+            accelaration=force_coord.T/mass_all
             #updating position
-            coords_new=coords_cart_all + vel*dt+ 0.5*accelaration*dt**2
+            coords_new=coords_cart_all + vel*dt+ 0.5*accelaration.T*dt**2
+            d_coord=coords_new-coords_cart_all
+            d_COM=np.sum(mass_all*d_coord.T, axis=1)/np.sum(mass_all)
+            coords_new-=d_COM.T
+
+            del d_coord, d_COM
+
 
             ##Calculating the new energy and forces
-            en_new, force_coord_new, rho=compute_en_force(coords_new, rho_md)
+            en_new, force_coord_new, rho=compute_en_force(dftcomm, coords_new, rho_md)
             rho_md=rho
+            force_coord_new*=RYDBERG
             ##Calculating the new velocity
             #region debug statement
             if comm.rank==0:
                 print("the force calculated at this position is", force_coord_new)
-                print("the extra accelaration coming from this force is", force_coord_new/mass_all)
-                print("the addition to velocity is ", 0.5*(accelaration+force_coord_new/mass_all)*dt)
+                print("the extra accelaration coming from this force is", force_coord_new.T/mass_all)
+                print("the addition to velocity is ", 0.5*(accelaration+force_coord_new.T/mass_all)*dt)
                 print("velocity before this addition is", vel)
             #endregion end of debug statement
-            vel=vel+0.5*(accelaration+force_coord_new/mass_all)*dt
+            vel=vel+0.5*(accelaration+force_coord_new.T/mass_all).T*dt
 
             #reassigning the forces and coordinates
             force_coord=force_coord_new
@@ -311,30 +323,38 @@ def Andersen_MD(dftcomm: DFTCommMod,
 
             #region debug statement for the velocity
             #Calculating the total energy and Temperature
-            ke=0.5*np.sum(mass_all*np.sum(vel**2, axis=1))
+            ke=0.5*np.sum(mass_all*vel.T**2)
             en_total=en_new+ke
             T_new=2*ke/(3*tot_num*BOLTZMANN_HART)
+
+            del accelaration, coords_new, force_coord_new
+
+
 
             ##printing all the variables
             #region debug statement
             if comm.rank==0:
+
                 print("the time is", time)
                 print("the temperature before scaling is", T_new)
                 print("the energy before scaling is ", en_total)
                 print("the ke energy before scaling is", ke)
             #endregion end of debug statement
 
-            prob=nu*dt
-            for atom in range(tot_num):
-                if np.random.rand()<prob:
-                    sigma=np.sqrt(kT/mass_si[atom])
-                    vel[atom]=np.random.normal(0, sigma, 3) ##This is in SI units
-                    vel[atom]/=vel_HART
-            print("velocity after random scaling", vel)
-            comm.Bcast(vel)
+            prob=float(nu)*dt
 
+            if comm.rank==0:
+                for atom in range(tot_num):
+                    b=np.random.rand()
+                    if b <prob:
+                        sigma=np.sqrt(kT/mass_si[atom])
+                        vel[atom]=np.random.normal(0, sigma, 3) ##This is in SI units
+                        vel[atom]/=vel_HART
+                print("velocity after random scaling", vel)
+            comm.Bcast(vel)
+            print("the velocity after the random scaling is", vel)
             #Calculating the total energy and Temperature
-            ke=0.5*np.sum(mass_all*np.sum(vel**2, axis=1))
+            ke=0.5*np.sum(mass_all*vel.T**2)
             en_total=en_new+ke
             T_new=2*ke/(3*tot_num*BOLTZMANN_HART)
 
@@ -348,20 +368,26 @@ def Andersen_MD(dftcomm: DFTCommMod,
             #endregion end of debug statement
 
             time_step=int(time/dt)
-            time_array.append(time_step)
-            energy_array.append(en_total)
-            temperature_array.append(T_new)
+            Har_to_ev=27.211386245988
+            time_array[time_step]=time
+            energy_array[time_step]=en_total*Har_to_ev
+            ke_array[time_step]=ke*Har_to_ev
+            pe_array[time_step]=en_new*Har_to_ev
+            temperature_array[time_step]=T_new
+
             time+=dt
 
         time_array=np.array(time_array)
         temperature_array=np.array(temperature_array)
         energy_array=np.array(energy_array)
+        ke_array=np.array(ke_array)
+        pe_array=np.array(pe_array)
 
         comm.Bcast(time_array)
         comm.Bcast(temperature_array)
         comm.Bcast(energy_array)
         comm.Bcast(coords_cart_all)
 
-    return coords_cart_all, time_array, temperature_array , energy_array
+    return coords_cart_all, time_array, temperature_array , energy_array, ke_array, pe_array
 
     
